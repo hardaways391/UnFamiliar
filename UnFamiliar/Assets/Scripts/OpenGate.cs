1c0268502ac5b7fe22b6717223f566297b3b4dc0
Jacob Tutor <tutorjacob@hotmail.com>
2023-02-06 03:16:34 -0600

    ScreenShake and Player Weight for Switches

--- UnFamiliar/Assets/Scripts/OpenGate.cs
+++ UnFamiliar/Assets/Scripts/OpenGate.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class OpenGate : MonoBehaviour
 {
     [SerializeField] private Animator gate = null;
+    CinemachineImpulseSource impulse;
 
+    private void Start()
+    {
+        impulse = GetComponent<CinemachineImpulseSource>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Weight"))
+        if (other.CompareTag("Weight") || other.CompareTag("Player"))
         {
             gate.Play("OpenGate", 0, 0f);
         }
@@ -17,9 +23,15 @@ public class OpenGate : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Weight"))
+        if (other.CompareTag("Weight") || other.CompareTag("Player"))
         {
             gate.Play("CloseGate", 0, 0f);
+            Shake();
         }
+    }
+
+    void Shake()
+    {
+        impulse.GenerateImpulse();
     }
 }
