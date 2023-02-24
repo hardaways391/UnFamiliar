49707c6342ae1aff122734e188d1899c5141f0b2
ZeroXTran <98497908+ZeroXTran@users.noreply.github.com>
2023-02-23 18:13:33 -0600

    Loading Screen Roughly
    gave the game a loading screen on the main menu.

---
+++ UnFamiliar/Assets/Scripts/Loading.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Loading : MonoBehaviour
+{
+   public GameObject LoadingScreen;
+   public Image LoadingBarFill;
+
+   public void LoadScene(int sceneId)
+   {
+
+    StartCoroutine(LoadSceneAsync(sceneId));
+ 
+   }
+
+   IEnumerator LoadSceneAsync(int sceneId)
+   {
+    AsyncOperation opreation = SceneManager.LoadSceneAsync(sceneId);
+
+    LoadingScreen.SetActive(true);
+
+    while (!opreation.isDone)
+    {
+    float progressValue = Mathf.Clamp01(opreation.progress / 0.9f);
+
+    LoadingBarFill.fillAmount = progressValue;
+
+    yield return null;
+   }
+   }
+}
